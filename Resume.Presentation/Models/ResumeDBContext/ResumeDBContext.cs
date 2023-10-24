967b7f8afd336bf3452ca9337ae9e3a9354d48ab
az.hosseini <az.hosseini@depna.com>
2023-10-24 22:32:35 +0330

    Config Database

---
+++ Resume.Presentation/Models/ResumeDBContext/ResumeDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Resume.Presentation.Models.ResumeDBContext
+{
+    public class ResumeDBContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=Resume;User Id=sa;Password=!@#123qwe;TrustServerCertificate=True");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
